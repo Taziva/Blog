4d016c832754ff3fcea69c7e3ee7acd9234d79de
Taziva <stmarovatsanga@gmail.com>
2018-01-08 22:55:34 +0000

    Replace Portfolio page with About Me page

---
+++ src/components/pages/AboutMe.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Card from "../layout/Card.jsx";
+import Layout from "../layout/Layout.jsx";
+
+export default class AboutMe extends Component {
+  render() {
+    return (
+      <Layout pageTitle="About Me" tagLine="If you wanna know me">
+        <Card sectionName="about-me" title="About Me" text={text}>
+          <img
+            className="about-me__media-image"
+            src="/images/hero-image.jpg"
+            alt="Shoniwa Marovatsanga"
+          />
+        </Card>
+      </Layout>
+    );
+  }
+}
+
+const text =
+  "I am currently working as a Software Engineer at Honeycomb TV, where I am able to learn from colleagues with different levels of experience.\n\n Outside of work I spend my time working on new web apps , and helping bring ideas to life. If you have any ideas that you think I could help bring to life don’t hesitate to get in touch.";
--- src/components/pages/AboutMe.test.js
+++ src/components/pages/AboutMe.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { StaticRouter } from "react-router-dom";
 
 import AboutMe from "./AboutMe.jsx";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<AboutMe />, div);
+  ReactDOM.render(
+    <StaticRouter context={{}}>
+      <AboutMe />
+    </StaticRouter>,
+    div
+  );
 });
