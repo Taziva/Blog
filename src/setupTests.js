a924c8b0e1b4009de28d028d2fe207f16d88e0e7
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2018-01-10 20:41:06 +0000

    Add Google Analytics (#16)

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,5 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { shallow, mount } from "enzyme";
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 
 configure({ adapter: new Adapter() });
