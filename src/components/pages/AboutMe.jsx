350a9cfe9b90260f824456140efcc30a988112c4
Taziva <stmarovatsanga@gmail.com>
2018-02-22 22:19:01 +0000

    Add Mission and About Me Add Simplifica font

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -6,7 +6,9 @@ export default class AboutMe extends Component {
   render() {
     return (
       <Layout pageTitle="About" tagLine="If you wanna know me">
-        <Card sectionName="about-me" title="About Me" text={text}>
+        <Card sectionName="mission" title="Mission" text={mission} />
+        <Card sectionName="concept" title="Concept" text={concept} />
+        <Card sectionName="about-me" title="About Me" text={shoniwa}>
           <img
             className="about-me__media-image"
             src="/images/hero-image.jpg"
@@ -17,6 +19,10 @@ export default class AboutMe extends Component {
     );
   }
 }
+const mission =
+  "OBM is a platform to showcase great ideas that have found a place in the world";
+const concept =
+  "The concept for Our Blind Mouse came from the way that life seems to often play out for visionaries, they have an idea and although they don't see it with their eyes they can see it in their minds. In pursuit of this vision they move by instinct, hope, and faith. Instinct is quite a wild animalistic sense, I felt the brand needed an animal to represent this however it had to be something that moved with hope and faith. <p> I then came across Matthew 5 v5:</p>\n\n <blockquote> Blessed are the meek, for they will inherit the earth. </blockquote>\n\n My animal had to be meek and led by instinct, it's survival based entirely around faith, so I chose a blind mouse.";
 
-const text =
-  "I am currently working as a Software Engineer at Honeycomb TV, where I am able to learn from colleagues with different levels of experience.\n\n Outside of work I spend my time working on new web apps , and helping bring ideas to life. If you have any ideas that you think I could help bring to life don’t hesitate to get in touch.";
+const shoniwa =
+  "My name's Shoniwa Marovatsanga and I currently run Our Blind Mouse as a seed project which I hope in future can be a place where ideas are born, hosted and nurtured.\n\n I am currently working as a Software Engineer at Honeycomb TV, where we do great work in video delivery and I am given a great environment to learn from colleagues with different levels of experience.\n\n Outside of work I spend my time working on new web apps , and helping bring ideas to life. If you have any ideas that you think I could help bring to life don’t hesitate to get in touch.";
